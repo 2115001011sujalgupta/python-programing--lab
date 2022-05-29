417b3b7fe4817a805a2ea94dcd3804d850693f17
2115001011sujalgupta <106176203+2115001011sujalgupta@users.noreply.github.com>
2022-05-29 12:33:37 +0530

    Create function question3.py

---
+++ function question3.py
@@ -0,0 +1,10 @@
+def max_min(a,b):
+    if a>b:
+        return a'is garter than 'b
+    return b'is grater than 'a
+
+
+x = int(input('enter the number :'))
+y = int(input('enter the  2nd number :'))
+totoal = max_min(x,y)
+print (totoal)
